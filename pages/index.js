f02c4db577ffeb06dfa13e23c0139df9ccc3c981
Azikenp <azikenp26@gmail.com>
2023-10-18 17:47:16 +0100

    nav done

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-primary/60 h-full">
+      {/* text */}
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          <h1 className="h1">
+            Transforming ideas <br /> Into <span className="text-accent">Digital reality</span>
+          </h1>
+          {/* subtitle */}
+          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">In the heart of a bustling city, where time never seems to slow down, there exists a hidden oasis of tranquility. The streets, filled with the aroma of freshly brewed coffee and the laughter of strangers, lead to unexpected adventures. </p>
+          {/* btn */}
+          <div className="flex justify-center xl:hidden relative">
+            <ProjectsBtn />
+          </div>
+        </div>
+      </div>
+      <div>image</div>
+    </div>
+  );
 };
 
 export default Home;
