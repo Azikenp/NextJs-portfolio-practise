7908e7bf5fa594da3c9fc6ae4c413a9164002978
Azikenp <azikenp26@gmail.com>
2023-10-18 23:52:53 +0100

    home page done

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,9 @@
+//next image
+import Image from 'next/image'
+import Link from 'next/link'
+import Socials from '../components/Socials'
+
+
 const Header = () => {
   return <header className="absolute z-30 w-full ">Headers</header>;
 };
