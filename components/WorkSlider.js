ab3df4da01509623a7e070214893ae611d9cee01
Azikenp <azikenp26@gmail.com>
2023-10-24 18:44:05 +0100

    final

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -83,7 +83,7 @@ const WorkSlider = () => {
                     className="relative rounded-lg overflow-hidden flex items-center justify-center group"
                     key={index}
                   >
-                    <div className="flex items-center justify-center relative overflow-hidden">
+                    <div className="flex items-center justify-center relative overflow-hidden group">
                       <Image
                         src={image.path}
                         width={500}
@@ -91,7 +91,22 @@ const WorkSlider = () => {
                         alt="image"
                       />
                       {/* overlay gradient */}
-                      <div className="absolute inset-0 bg-gradient-to-l from transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+                      <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-300"></div>
+                      {/* title */}
+                      <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
+                        <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                          {/* title part 1 */}
+                          <div className="delay-100">LIVE</div>
+                          {/* title part 2 */}
+                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
+                            PROJECT
+                          </div>
+                          {/* icon */}
+                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
+                            <BsArrowRight />
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 );
